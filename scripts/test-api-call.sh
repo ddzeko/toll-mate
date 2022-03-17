afa453c3a86dbf2aecfeadffd28bf3492f592ad9
ddzeko <ddzeko@gmail.com>
2022-03-17 23:42:25 +0100

    test script revised

--- scripts/test-api-call.sh
+++ scripts/test-api-call.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-curl -sS -X POST http://127.0.0.1:5000/api/routeInfo \
+
+testUri=/api/routeInfo
+
+if [[ -z "$PYTHONANYWHERE_DOMAIN" ]]; then
+    testSite="http://127.0.0.1:5000"
+else
+    testSite=${WSGI_MODULE%%_wsgi}
+    testSite=${testSite//_/.}
+fi
+
+rm -f response.head response.body 2>&- >&-
+curl -sS -X POST "${testSite}${testUri}" \
   -H "Content-Type: application/json" \
-  -d '{"hac_ulaz": "Karlovac", "hac_izlaz": "Vučevica"}' \
+  -d '{ "hac_ulaz": "Karlovac", "hac_izlaz": "Vučevica" }' \
   -D response.head \
   -o response.body
