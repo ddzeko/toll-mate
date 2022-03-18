9c24adc4875ba62ae38adfba2e38250e4b8ae014
ddzeko <ddzeko@gmail.com>
2022-03-18 18:32:41 +0100

    updated db backup script

--- scripts/dump-hac-tables.sh
+++ scripts/dump-hac-tables.sh
@@ -12,11 +12,15 @@ if [[ -z "$DATABASE_HOST" || -z "$DATABASE_USER" || -z "$DATABASE_NAME" ]]; then
     exit 1
 fi
 
-# if GTID used, add option: --set-gtid-purged=OFF
-# for MySQL, add --column-statistics=0
+if [[ -z "$PYTHONANYWHERE_DOMAIN" ]]; then
+    pa_options=""
+else
+    # options applicable for PythonAnywhere
+    pa_options="--set-gtid-purged=OFF --column-statistics=0"
+fi
 
 mysqldump -u ${DATABASE_USER} -h ${DATABASE_HOST} -P ${DATABASE_PORT} \
-    --no-tablespaces --complete-insert --skip-extended-insert \
+    --no-tablespaces --complete-insert --skip-extended-insert $pa_options \
     "${DATABASE_NAME}" \
     hac_point hac_mjesto hac_tablica_ruta \
 | xz -9 > hac.dump.sql.xz
